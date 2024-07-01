7bf40494b605a282417a015fae26cbb0ef519921
Jay <151563355+CubeLab1@users.noreply.github.com>
2024-07-01 13:30:06 -0600

    Create hello_gradescope.py

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Jesus Martinez 
+# OSU Email: martjes6@oregonstate.com
+# Course: CS261 - Data Structures
+# Assignment: hello_gradescope
+# Due Date: 7/1/2024
+# Description: First assignment to check uploading to gradescope
+
+
+def gradescope_test():
+    return 42
